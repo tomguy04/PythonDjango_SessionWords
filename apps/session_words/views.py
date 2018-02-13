e34c2516ed029b9bd06f9a20281d177ca44842e4
Glen Goulter <glenrg@gmail.com>
2018-02-12 19:38:07 -0800

    first version, lacks colors and there is a bug in handling the history logic.

---
+++ apps/session_words/views.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+# the index function is called when root is visited
+def index(request):
+    #return the form.
+    return render(request,"session_words/form.html")
+def process(request):
+    request.session.modified = True
+    houseDict = {}
+    request.session['words'] = request.POST['name']
+    # try:
+    #     request.session['history']
+    #     print "history is here"
+    # except KeyError:
+    #     request.session['history'] = []
+    # print "made it"
+    # houseDict['dictWord'] = (request.POST['name'])
+    # request.session['history'].append(request.POST['name'])
+
+    # if 'history' not in request.session.keys():
+    #     request.session['history'] = []
+    # print "this line"        
+    # houseDict['dictWord'] = (request.POST['name'])
+    # request.session['history'].append(request.POST['name'])
+    # print "**********************history->"+str(request.session['history'])
+    # return redirect('/session_words')
+        
+    if 'history' in request.session.keys():
+        houseDict['dictWord'] = (request.POST['name'])
+        request.session['color'] = request.POST['color']
+        request.session['history'].append(houseDict)
+        print "**********************MOREhistory->"+str(request.session['history'])
+        
+    else:
+        request.session['history'] = []
+        houseDict['dictWord'] = (request.POST['name'])
+        request.session['history'].append(request.POST['name'])
+        print "**********************history->"+str(request.session['history'])
+        
+    return redirect('/session_words')
+
+    
