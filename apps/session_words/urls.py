f6e2910d2461263393aa1bb096ad66cffb343aa6
Glen Goulter <glenrg@gmail.com>
2018-02-12 20:55:11 -0800

    the color coding and logic are fixed, and added the clear button.  Needs a bit more css but proof of concept.

--- apps/session_words/urls.py
+++ apps/session_words/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
+  url(r'^session_words/clear$', views.clear),
   url(r'^session_words/process$', views.process),
   url(r'^session_words/$', views.index)     # This line has changed!
 ]
--- apps/session_words/views.py
+++ apps/session_words/views.py
@@ -2,22 +2,25 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
+from datetime import datetime
 # the index function is called when root is visited
 def index(request):
     #return the form.
     return render(request,"session_words/form.html")
 def process(request):
-    request.session.modified = True
+    #request.session.modified = True
     houseDict = {}
     request.session['words'] = request.POST['name']
-    # try:
-    #     request.session['history']
-    #     print "history is here"
-    # except KeyError:
-    #     request.session['history'] = []
-    # print "made it"
-    # houseDict['dictWord'] = (request.POST['name'])
-    # request.session['history'].append(request.POST['name'])
+    try:
+        request.session['history']
+        print "history is here"
+    except KeyError:
+        request.session['history'] = []
+    print "made it"
+    houseDict['dictWord'] = (request.POST['name'])
+    houseDict['color'] = (request.POST['color'])
+    houseDict['date'] = datetime.now().strftime("%Y/%m/%d at %I:%m:%p")
+    request.session['history'].append(houseDict)
 
     # if 'history' not in request.session.keys():
     #     request.session['history'] = []
@@ -27,18 +30,26 @@ def process(request):
     # print "**********************history->"+str(request.session['history'])
     # return redirect('/session_words')
         
-    if 'history' in request.session.keys():
-        houseDict['dictWord'] = (request.POST['name'])
-        request.session['color'] = request.POST['color']
-        request.session['history'].append(houseDict)
-        print "**********************MOREhistory->"+str(request.session['history'])
+    # if 'history' in request.session.keys():
+    #     houseDict['dictWord'] = (request.POST['name'])
+    #     houseDict['color'] = (request.POST['color'])
+    #     houseDict['date'] = datetime.now().strftime("%Y/%m/%d at %I:%m:%p")
+    #     request.session['history'].append(houseDict)
+    #     print "**********************MOREhistory->"+str(request.session['history'])
         
-    else:
-        request.session['history'] = []
-        houseDict['dictWord'] = (request.POST['name'])
-        request.session['history'].append(request.POST['name'])
-        print "**********************history->"+str(request.session['history'])
-        
+    # else:
+    #     houseDict['dictWord'] = (request.POST['name'])
+    #     houseDict['date'] = datetime.now().strftime("%Y/%m/%d at %I:%m:%p")
+    #     request.session['history'] = []
+    #     request.session['color'] = request.POST['color']
+    #     request.session['history'].append(request.POST['name'])
+    #     print "**********************history->"+str(request.session['history'])
+    request.session.modified = True    
     return redirect('/session_words')
+
+def clear(request):
+    request.session.clear()
+    return redirect('/session_words')
+
 
     
